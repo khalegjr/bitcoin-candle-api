29c51a285d0e7e43a6f69c424c2739676185df89
Khaled <khalegjr@gmail.com>
2022-08-14 19:45:04 -0300

    criando o endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import * as express from "express";
 import * as logger from "morgan";
 import * as cors from "cors";
+import { candleRouter } from "./routes/candles";
 
 export const app = express();
 app.use(cors()); // para liberar o acesso geral
 app.use(express.json()); // para receber e devolver as requisições em json
 app.use(logger("dev")); // um log com um pouco mais de detalhes
+
+app.use("/candles", candleRouter);
---
+++ src/routes/candles.ts
@@ -0,0 +1,12 @@
+import CandleController from "src/controllers/CandleController";
+import { Request, Response, Router } from "express";
+
+export const candleRouter = Router();
+const candleCtrl = new CandleController();
+
+candleRouter.get("/:quantity", async (req: Request, res: Response) => {
+  const quantity = parseInt(req.params.quantity);
+  const lastCandles = await candleCtrl.findLastCandles(quantity);
+
+  return res.json(lastCandles);
+});
